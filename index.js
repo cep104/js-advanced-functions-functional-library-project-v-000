50cc551896162e6e6177625233fbc97dec271942
cep104 <candicelizabethpeters@gmail.com>
2020-12-03 10:55:45 -0600

    commit tree cb4954e9ae065f21f6627a8db3ebacd492b358dc

--- index.js
+++ index.js
@@ -173,70 +173,232 @@ const fi = (function() {
     // } how you can compare numbers using sort. 
 
  
-        flatten: function flatten(array, shallow){
-      let newArr = []
-      for(var i in array){
-        if (shallow === true){
-          // console.log(array)
-          if(Array.isArray(array[i])) {
-            // console.log(array[i])
-              newArr = newArr.concat(array[i])
-          } else {
-              newArr.push(array[i])
-          }
-        }
-        else {
-          if(Array.isArray(array[i])) {
-            console.log(flatten(array[i]))
-              newArr = newArr.concat(flatten(array[i]))
-          } else {
-              newArr.push(array[i])
-          }
-        }
-      }
-      return newArr
-    },
-
-       // unpack: function(receiver, arr) {
-    //   for (let val of arr)
-    //     receiver.push(val)
-    // },
-
-    // flatten: function(collection, shallow, newArr=[]) {
-    //   console.log(shallow)
-    //   if (!Array.isArray(collection)) return newArr.push(collection)
-    //   if (shallow) {
-    //     for (let val of collection)
-    //       Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
-    //   } else {
-    //     for (let val of collection) {
-    //       this.flatten(val, false, newArr)
+    //     flatten: function flatten(array, shallow){
+    //   let newArr = []
+    //   for(var i in array){
+    //     if (shallow === true){
+    //       // console.log(array)
+    //       if(Array.isArray(array[i])) {
+    //         // console.log(array[i])
+    //           newArr = newArr.concat(array[i])
+    //       } else {
+    //           newArr.push(array[i])
+    //       }
+    //     }
+    //     else {
+    //       if(Array.isArray(array[i])) {
+    //         console.log(flatten(array[i]))
+    //           newArr = newArr.concat(flatten(array[i]))
+    //       } else {
+    //           newArr.push(array[i])
+    //       }
     //     }
     //   }
     //   return newArr
     // },
+
+    //    unpack: function(receiver, arr) {
+    //      //takes in the array (arr) and the new array(receiver)
+    //   for (let val of arr)
+    //   //let each value of the array be pushed into the new array
+    //     receiver.push(val)
+    // },
+    flatten: function(array, shallow){
+      
+      const shallow_flatten = (array) => {
+        let res = []
+        //console.log(array)
+        for (const x in array){
+          // console.log(array[x]) look at what each value in the array is 
+          if (Array.isArray(array[x])){
+            //if array[x] is an array
+            // console.log(array[x]) arrays that are arrays
+            res = res.concat(array[x])
+            // console.log(res)
+          } else {
+            res.push(array[x])
+            // console.log(res) if  array[x] is not an array
+          }
+        }
+        //console.log(res) return the array  with one nest taken away
+        return res
+      }
+
+      const full_flatten = array => {
+        let res = []
+
+        for (const x in array){
+          if (Array.isArray(array[x])){
+            //if array[x] is an array
+            // console.log(array[x])
+            // console.log(full_flatten(array[x]))
+            //runs through arrays till they are all in one array 
+            res = res.concat(full_flatten(array[x]))
+            
+            // console.log(res)
+          } else {
+            res.push(array[x]) 
+            //if not in an array push into new array
+          }
+        }
+
+        return res
+        //return new array 
+      }
+
+      if (shallow){
+        return shallow_flatten(array)
+      } else{
+        return full_flatten(array)
+      }
+
+    },
+    // flatten: function(collection, shallow, newArr=[]) {
+
+    //   // console.log(shallow)
+    //   if (!Array.isArray(collection)) {
+    //     // console.log(collection) ex. 1 or 2 or 3
+    //    return newArr.push(collection) //return [1]
+    //     // if collection is not an array return the new array with collection pushed inside
+    //   }
+    //   if (shallow) {
+    //     //if shallow = true
+    //     for (let val of collection)
+    //       Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
+    //       //if the val of the collection is an array then run it through unpack 
+    //       // otherwise if it is not an array put it inside newArr
+    //   } else {
+    //     //if it's an array and doesn't have a shallow 
+    //     for (let val of collection) {
+    //       //run the value of the collection through the flatten function. 
+    //       this.flatten(val, false, newArr)
+    //     }
+    //   }
+    //   return newArr
+    //   //return the new array 
+    // },
    
     // Produces a duplicate-free version of the array, using === to test object equality. 
     // In particular only the first occurrence of each value is kept.
-
-
-     uniq: function(array, isSorted, callback) {
-      let uniqueArray
-      if (callback) {
-        let arr = [... array].map(element => callback(element))
-        uniqueArray = array.filter( (value, index, array) => arr.indexOf(callback
-          
-          (value)) === index);
-      } else {
-        uniqueArray = [...new Set(array)]
+    uniqSorted: function(collection, callback) {
+      // console.log(collection)
+      const sorted = [collection[0]]
+      //collection[0] is first element of collection array
+      for (let i = 1; i < collection.length; i++) {
+        if (sorted[i-1] !== collection[i])
+        //if second element from collection doesn't equal 
+        //the new element from sorted add that element to the sorted array
+        //sorted starts off with the first element in the collection
+        //itterates through each time so we are checking the next element that has been put in sorted vs. the next element in the collection.
+        //example [1,2,2,3,4] is collection than sorted would be [1]
+        // so we would check sorted[1] against collection[1] which is [2] and so on. if any of them match it won't put it inside
+          sorted.push(collection[i])
       }
-      return uniqueArray;
+      return sorted
     },
+
+    // uniq: function(collection, sorted=false, callback=false) {
+    //   // console.log(collection)
+    //   // [ 1, 1, 2, 3, 2, 4, 5, 6, 1 ] unsorted
+    //   // [ { a: 1, b: 2 }, { c: 3, d: 4 }, { a: 1, b: 2 } ]
+    //    //[1, 2, 2, 3,4, 6, 9]
+    //    console.log(collection[0])
+    //   if (sorted) {
+    //     // console.log(collection)
+        
+    //     return fi.uniqSorted(collection, callback)
+    //   } else if (!callback) {
+    //     return Array.from(new Set(collection))
+    //   } else {
+    //     const modifiedVals = new Set()
+    //     const uniqVals = new Set()
+    //     for (let val of collection) {
+    //       const moddedVal = callback(val)
+    //       if (!modifiedVals.has(moddedVal)) {
+    //         modifiedVals.add(moddedVal)
+    //         uniqVals.add(val)
+    //       }
+    //     }
+    //     return Array.from(uniqVals)
+    //   }
+    // },
+
+    uniq: function(array, isSorted=false, callback = (x)=>x){
+      let uniqArray = [array.shift()]
+      console.log(uniqArray)
+      if(isSorted===false){
+        for(const element of array){
+          callback(element)===callback(uniqArray[uniqArray.length-1])? Array :uniqArray.push(element)
+        }
+      }else{
+        for(const element of array){
+          for(const e of uniqArray){
+            if(callback(e)===callback(element)){
+              return uniqArray
+            }
+            }
+            uniqArray.push(element) 
+            }
+          return uniqArray
+          }
+
+        },
+    // uniq: function(array, isSorted, callback){
+    //   let set = []
+    //   callback = callback || (x => x)
+    //   // console.log(callback)
+    //   if (!isSorted){
+    //     const includes = function(e,callback){
+    //       // console.log(this) this is a list of arrays
+    //       for (const x in this){
+    //         //this is list of arrays 
+    //         // x is each array 
+    //         // console.log(x) 
+    //         // console.log(e)
+    //         if(callback(this[x])===callback(e)){
+    //           return true
+    //         }
+    //       }
+    //       return false
+    //     }
+
+    //     for (const x in array){
+    //       if (!includes.call(set,array[x],callback)){
+    //         set.push(array[x])
+    //       }
+    //     }
+    //   } else{
+    //     for (let i = 0; i < array.length; i++){
+    //       set.push(array[i])
+
+    //       while(callback(array[i])===callback(array[i+1])){
+    //         i++
+    //       }
+    //     }
+    //   }
+
+    //   return set
+    // },
+    //  uniq: function(array, isSorted, callback) {
+    //   let uniqueArray
+    //   if (callback) {
+    //     let arr = [... array].map(element => callback(element))
+    //     uniqueArray = array.filter( (value, index, array) => arr.indexOf(callback
+          
+    //       (value)) === index);
+    //   } else {
+    //     uniqueArray = [...new Set(array)]
+    //   }
+    //   return uniqueArray;
+    // },
     // Retrieve all the names of the object's own enumerable properties.
     keys: function(object) {
+      // console.log(object)
+      // { one: 1, two: 2, three: 3, four: 4 }
       let keysArray = []
 
       for (const key in object) {
+        // console.log(key)
         keysArray.push(key) 
       }
 
@@ -257,8 +419,11 @@ const fi = (function() {
     // returns a sorted collection of the names of every method in an object
     functions: function(object) {
       let sortedArray = []
-      
+      // console.log(object)
+      // { a: '', z: [Function: z], p: '', c: [Function: c], k: [Function: k] }
       for (const key in object) {
+        // console.log(object[key])
+        // [Function: z]
         if (typeof object[key] === 'function') {
           sortedArray.push(key)
         }
